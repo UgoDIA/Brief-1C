c3be87497f73f1eef491d76970c52f7187f7e741
DevIA.E03 <DevIA.E03@afpar.com>
2022-11-21 11:28:27 +0400

    configuration

--- src/ETL/settings.py
+++ src/ETL/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ETL'
+    'identification'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'ETL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testETL', 
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
