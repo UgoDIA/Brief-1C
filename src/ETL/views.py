04253ba8faaf3a77c1a44c8c7e7f3c252ed0aa7d
UgoDIA <ugofokyin@gmail.com>
2022-12-05 15:00:47 +0400

    Feedback fini

--- src/ETL/views.py
+++ src/ETL/views.py
@@ -33,70 +33,96 @@ def uploadCsv(request):
         listeFichiers=[f for f in listdir(monRepertoire) if isfile(join(monRepertoire,f))]
         urlFichiers=[monRepertoire + f for f in listeFichiers]
         context['noms']=listeFichiers
+        original=[]
+        duppli=[]
+        avoirs=[]
+        dates=[]
+        produits=[]
+        quant=[]
+        pays=[]
+        total=[]
+        pourcent=[]
         # delete(monRepertoire)
-        df = pd.read_csv(urlFichiers[0],encoding = "ISO-8859-1")                             
-        print('original : '+str(df.shape))
-        df.rename(columns={'InvoiceNo':'noFacture','StockCode':'codeProduit','Description':'nomProduit','InvoiceDate':'dateFacture','Country':'pays'},inplace=True)
-        df.drop(['CustomerID','UnitPrice'], inplace=True, axis=1)
-        print('suppr colonne : '+str(df.shape))                        
-        df=df.drop_duplicates(['noFacture','codeProduit']) 
-        print('suppr dupli : '+str(df.shape)) 
-        df['noFacture']=pd.to_numeric(df['noFacture'],errors='coerce') 
-        df.dropna(subset=['noFacture'], inplace = True)
-        print('suppr avoirs : '+str(df.shape))
-        df['dateFacture']=pd.to_datetime(df['dateFacture'], format='%m/%d/%Y %H:%M',errors='coerce')
-        df.dropna(subset=['dateFacture'], inplace = True)
-        print('suppr fdate :'+str(df.shape))
-        df.loc[(df['codeProduit'].str.len()<=4 )&( df['codeProduit'].str.len()>2) ]=np.nan
-        df.loc[df['codeProduit'].str.len()>8 ]=np.nan
-        df.dropna(inplace = True)
-        print('suppr codes : '+str(df.shape))
-        df=df[(df['Quantity']>0 |(df['Quantity'].isnull()))]
-        df.drop(['Quantity'], inplace=True, axis=1)
-        print('suppr qté n :'+str(df.shape))
-        df.drop(df[df['pays']== 'Unspecified'].index,inplace=True)
-        print('suppr unsp :'+str(df.shape))
-        listePays=df.drop_duplicates('pays').copy()
-        # listePays.drop(['noFacture','dateFacture','codeProduit','nomProduit','dateFacture'], inplace=True, axis=1)
-        # listePays.to_sql('pays',engine, if_exists='append',index= False)
-        P=listePays['pays'].to_list()
-        print('total pays : '+str(len(P)))     
-        for i in range(len(P)):
-            newPays=Pays(pays=P[i])
-            newPays.save()
-        listeFacture=df.drop_duplicates(subset=['noFacture']).copy()
-        listeFacture.drop(['codeProduit','nomProduit'], inplace=True, axis=1)
-        try:  
-            listeFacture.to_sql('ventes',engine, if_exists='append',index= False)
-        except:
-            pass    
-        # for index, row in listeFacture.iterrows():
-        #     newFacture=Ventes(nofacture=row['noFacture'],datefacture=row['dateFacture'],pays=Pays.objects.get(pays=row['pays']))
-        #     newFacture.save()
-        df.drop(['dateFacture','pays'], inplace=True, axis=1)   
-        listeProduits=df.drop_duplicates(subset=['codeProduit'])
-        for index, row in listeProduits.iterrows():
-            newProduits=Produits(codeproduit=row['codeProduit'],nomproduit=row['nomProduit'])
-            newProduits.save()
-        df.drop(['nomProduit'], inplace=True, axis=1) 
-        # for index, row in df.iterrows():
-        #     newDetails=Detailsventes(nofacture=Ventes.objects.get(nofacture=row['noFacture']),codeproduit=Produits.objects.get(codeproduit=row['codeProduit']))
-        #     newDetails.save()
-        
-        try:
-            df.to_sql('detailsVentes',engine, if_exists='append',index= False)
-        except:
-            pass
-    return render(request,'uploadCsv.html', context)
+        for i in range(len(listeFichiers)):
+            df = pd.read_csv(urlFichiers[i],encoding = "ISO-8859-1")                            
+            original.append(len(df))
+            context['original']=original
+            df.rename(columns={'InvoiceNo':'noFacture','StockCode':'codeProduit','Description':'nomProduit','InvoiceDate':'dateFacture','Country':'pays'},inplace=True)
+            df.drop(['CustomerID','UnitPrice'], inplace=True, axis=1)
+            # print('suppr colonne : '+str(df.shape))                        
+            df=df.drop_duplicates(['noFacture','codeProduit']) 
+            # print('suppr dupli : '+str(df.shape))
+            duppli.append(len(df))
+            context['duppli']=duppli
+            df['noFacture']=pd.to_numeric(df['noFacture'],errors='coerce') 
+            df.dropna(subset=['noFacture'], inplace = True)
+            # print('suppr avoirs : '+str(df.shape))
+            avoirs.append(len(df))
+            context['avoirs']=avoirs
+            df['dateFacture']=pd.to_datetime(df['dateFacture'], format='%m/%d/%Y %H:%M',errors='coerce')
+            df.dropna(subset=['dateFacture'], inplace = True)
+            # print('suppr fdate :'+str(df.shape))
+            dates.append(len(df))
+            context['dates']=dates
+            df.loc[(df['codeProduit'].str.len()<=4 )&( df['codeProduit'].str.len()>2) ]=np.nan
+            df.loc[df['codeProduit'].str.len()>8 ]=np.nan
+            df.dropna(inplace = True)
+            # print('suppr codes : '+str(df.shape))
+            produits.append(len(df))
+            context['produits']=produits
+            df=df[(df['Quantity']>0 |(df['Quantity'].isnull()))]
+            df.drop(['Quantity'], inplace=True, axis=1)
+            # print('suppr qté n :'+str(df.shape))
+            quant.append(len(df))
+            context['quant']=quant
+            df.drop(df[df['pays']== 'Unspecified'].index,inplace=True)
+            # print('suppr unsp :'+str(df.shape))
+            pays.append(len(df))
+            context['pays']=pays
+            total.append(len(df))
+            context['total']=total
+            listePays=df.drop_duplicates('pays').copy()
+            listePays.drop(['noFacture','dateFacture','codeProduit','nomProduit','dateFacture'], inplace=True, axis=1)
+            P=listePays['pays'].to_list()
+            # print('total pays : '+str(len(P)))     
+            for i in range(len(P)):
+                newPays=Pays(pays=P[i])
+                newPays.save()
+            listeFacture=df.drop_duplicates(subset=['noFacture']).copy()
+            listeFacture.drop(['codeProduit','nomProduit'], inplace=True, axis=1)
+            try: 
+                listeFacture.to_sql('ventes',engine, if_exists='append',index= False)
+            except:
+                pass
+            # for index, row in listeFacture.iterrows():
+            #     newFacture=Ventes(nofacture=row['noFacture'],datefacture=row['dateFacture'],pays=Pays.objects.get(pays=row['pays']))
+            #     newFacture.save()
+            df.drop(['dateFacture','pays'], inplace=True, axis=1)
+            listeProduits=df.drop_duplicates(subset=['codeProduit']).copy()
+            listeProduits.drop(['noFacture'], inplace=True, axis=1)
+            for index, row in listeProduits.iterrows():
+                newProduits=Produits(codeproduit=row['codeProduit'],nomproduit=row['nomProduit'])
+                newProduits.save()
+            df.drop(['nomProduit'], inplace=True, axis=1)
+            # for index, row in df.iterrows():
+            #     newDetails=Detailsventes(nofacture=Ventes.objects.get(nofacture=row['noFacture']),codeproduit=Produits.objects.get(codeproduit=row['codeProduit']))
+            #     newDetails.save()
+            try:
+                df.to_sql('detailsVentes',engine, if_exists='append',index= False)
+            except:
+                pass
+        sub=np.subtract(original,total)
+        pourcent=[k/m for k,m in zip(sub,original)]
+        pourcent=[p *100 for p in pourcent]
+        pourcent=[round(p,2)for p in pourcent]
+        context['sub']=sub
+        context['pourcent']=pourcent
+        # print(context) 
+    return render(request,'uploadCsv.html', context=context)
 
 def delete(dossier):
     for root, dirs, files in os.walk(dossier):
         for f in files:
             os.unlink(os.path.join(root, f))
         for d in dirs:
-
             shutil.rmtree(os.path.join(root, d))
-            
-# SELECT setval('public."detailsVentes_idDetails_seq"', 0, true);
-# Truncate table pays CASCADE;
-# Truncate table produits CASCADE;
